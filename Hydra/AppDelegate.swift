b872ab144df6d4cec01a2e4524fb5cb5b4835ae8
Fool <foolofshadows@gmail.com>
2019-02-06 11:29:39 -0600

    Added icon from <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>

--- Hydra/AppDelegate.swift
+++ Hydra/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Fool on 2/6/19.
 //  Copyright © 2019 Fool. All rights reserved.
 //
+// <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 import Cocoa
 
