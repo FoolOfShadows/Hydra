3c0efed2801fcbb2a479ddd81a6dda01926032c8
Fool <foolofshadows@gmail.com>
2019-02-06 08:52:45 -0600

    Initial Commit

---
+++ Hydra/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hydra
+//
+//  Created by Fool on 2/6/19.
+//  Copyright © 2019 Fool. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
